109270d092c7b3abf1db51f31b2a3f8a95bbf397
Riz Michael Sim <rizsim214@gmail.com>
2020-07-10 19:02:32 +0800

    updated 10/07/2020

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -15,36 +15,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 <nav class="navbar navbar-dark navbar-expand-lg ">
+<div class="container">
   <a class="navbar-brand mb-0 h1" href="<?php echo base_url();?>">
       Customs
-       <img src="<?php echo base_url();?>assets/img/logo.png" class="d-inline-block align-top" width="35" height="35" alt="logo" loading="jof_logo">
+        <img src="<?php echo base_url();?>assets/img/logo.png" class="d-inline-block align-top" width="35" height="35" alt="logo" loading="jof_logo">
       Brokerage
   </a>
- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
  <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">FAQs</a>
-      </li>
-      <li class="nav-item dropdown">
+     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Services
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown">
-          <a class="dropdown-item" href="#">Set   Appointment</a>
-          <a class="dropdown-item" href="#">Terms & Agreement</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Contact US</a>
+          <a class="dropdown-item" href="#">Set Appointment</a>
+          <a class="dropdown-item" href="#">Request a Qoute</a>
+          <a class="dropdown-item" href="#">Consultations</a>
+          <a class="dropdown-item" href="#">Air Shipping</a>
+          <a class="dropdown-item" href="#">Sea Shipping</a>
         </div>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Support</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">News</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact US</a>
       </li>
     </ul>
 </div>
 
-<a class="btn btn-md btn-light">Sign In</a>
-
+<a href="#" class="btn btn-md btn-light">Sign In</a>
+</div>
 </nav>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,51 @@
 
 
-<div id="container">
-	<h1>HELLO WORLDUUU!!</h1>
+<div class="container my-3">
+	<div class="container-fluid col-lg-12 mx-auto" id="carouselContainer" >
+		<div id="MagicCarousel" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#MagicCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#MagicCarousel" data-slide-to="1"></li>
+			<li data-target="#MagicCarousel" data-slide-to="2"></li>
+			<li data-target="#MagicCarousel" data-slide-to="3"></li>
+			<li data-target="#MagicCarousel" data-slide-to="4"></li>
+			<li data-target="#MagicCarousel" data-slide-to="5"></li>
+		</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<img class="d-block w-100 h-100 " src="<?php echo base_url();?>assets/img/shipping1.jpg" alt="First Slide" style="max-height:500px" >		
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100 h-100" src="<?php echo base_url();?>assets/img/shipping2.png" alt="Second Slide" style="max-height:500px">
+						
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100 h-100" src="<?php echo base_url();?>assets/img/shipping3.jpg" alt="Third Slide"style="max-height:500px" >
+						
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100 h-100" src="<?php echo base_url();?>assets/img/shipping4.jpg" alt="Fourth Slide" style="max-height:500px">
+					
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100 h-100"  src="<?php echo base_url();?>assets/img/shipping5.jpg" alt="Fifth Slide" style="max-height:500px">
+						
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100 h-100" src="<?php echo base_url();?>assets/img/shipping7.jpg" alt="Sixth Slide" style="max-height:500px">
+					
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#MagicCarousel" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#MagicCarousel" role="button" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
 
 
 </div>
