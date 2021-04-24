1781c10f74532fb51fa6b79936a1b3803c6174ab
Luke Gayo <odemkeenen12@gmail.com>
2021-04-24 12:03:35 +0800

    update

--- application/views/consignee/includes/login_header.php
+++ application/views/consignee/includes/login_header.php
@@ -38,9 +38,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </li> 
 
         
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                <a class="nav-link" href="#" data-toggle="modal" data-target="#optionModal" id="navItem">Services</a>
-            </li> 
+            </li>  -->
              <li class="nav-item">
                  <!-- Default dropleft button -->
                 <div class="btn-group dropleft">
